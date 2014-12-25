f6bbb07d30842e3cce57429534ee5427a45a3751
gstraube <georg.straube@googlemail.com>
2014-12-25 20:30:42 +0100

    Solution for the 'Stacking Boxes' problem.

---
+++ src/programming_problems/StackingBoxes.java
@@ -0,0 +1,173 @@
+package programming_problems;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
+import static java.util.Collections.sort;
+
+class StackingBoxes {
+
+    static final Comparator<Box> BOX_COMPARATOR = new Comparator<Box>() {
+
+        @Override
+        public int compare(Box box1, Box box2) {
+            int product1 = 1;
+            int product2 = 1;
+
+            //Both boxes share the same number of dimensions
+            for (int i = 0; i < box1.measurements.length; i++) {
+                product1 *= box1.measurements[i];
+                product2 *= box2.measurements[i];
+            }
+
+            return Integer.compare(product1, product2);
+        }
+    };
+
+    private List<Box> longestStringOfBoxes = new ArrayList<>();
+
+    public void addBox(Box box) {
+        this.boxes.add(box);
+    }
+
+    private List<Box> boxes = new ArrayList<>();
+
+    public String boxesInLongestString() {
+        StringBuffer out = new StringBuffer();
+
+        for (int i = 0; i < this.longestStringOfBoxes.size(); i++) {
+            out.append(this.longestStringOfBoxes.get(i).index);
+            if (i < this.longestStringOfBoxes.size() - 1) {
+                out.append(" ");
+            }
+        }
+
+        return out.toString();
+    }
+
+    public int numberOfBoxesInLongestString() {
+        return this.longestStringOfBoxes.size();
+    }
+
+    private void sortBoxes() {
+        sort(this.boxes, BOX_COMPARATOR);
+    }
+
+    private boolean canBoxBeNested(Box boxToBeNested, Box containerBox) {
+        for (int i = 0; i < boxToBeNested.measurements.length; i++) {
+            if (boxToBeNested.measurements[i] >= containerBox.measurements[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void findLongestNestingString() {
+        int maxLength = 0;
+
+        this.sortBoxes();
+
+        for (int i = 0; i < this.boxes.size(); i++) {
+            List<Box> stringOfBoxes = this.getNestingStringStartingFromBoxIndex(i);
+            int stringOfBoxesLength = stringOfBoxes.size();
+            if (stringOfBoxesLength > maxLength) {
+                this.longestStringOfBoxes = stringOfBoxes;
+                maxLength = stringOfBoxesLength;
+            }
+        }
+    }
+
+    private List<Box> getNestingStringStartingFromBoxIndex(int startBoxIndex) {
+        int currentBoxIndex = startBoxIndex;
+        int nextBoxIndex = startBoxIndex + 1;
+
+        List<Box> stringOfBoxes = new ArrayList<>();
+
+        stringOfBoxes.add(this.boxes.get(currentBoxIndex));
+
+        while (nextBoxIndex < this.boxes.size()) {
+            Box currentBox = this.boxes.get(currentBoxIndex);
+            Box nextBox = this.boxes.get(nextBoxIndex);
+            Arrays.sort(currentBox.measurements);
+            Arrays.sort(nextBox.measurements);
+
+            if (this.canBoxBeNested(currentBox, nextBox)) {
+                stringOfBoxes.add(nextBox);
+                currentBoxIndex = nextBoxIndex;
+            }
+            nextBoxIndex++;
+        }
+
+        return stringOfBoxes;
+    }
+
+}
+
+class Box {
+
+    int index;
+    int[] measurements;
+
+    public Box(int numberOfDimensions) {
+        this.measurements = new int[numberOfDimensions];
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer out = new StringBuffer();
+        for (int i = 0; i < this.measurements.length; i++) {
+            out.append(this.measurements[i]);
+            out.append(" ");
+        }
+        return out.toString();
+    }
+
+}
+
+class SBMain {
+
+    public static void main(String[] args) {
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("in.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        PrintWriter out = new PrintWriter(System.out, true);
+
+        while (in.hasNextLine()) {
+
+            String firstLine = in.nextLine();
+            String[] specification = firstLine.split("\\s+");
+
+            int numberOfBoxes = Integer.parseInt(specification[0]);
+            int numberOfDimensions = Integer.parseInt(specification[1]);
+
+            StackingBoxes stackingBoxes = new StackingBoxes();
+
+            for (int i = 0; i < numberOfBoxes; i++) {
+                String boxLine = in.nextLine();
+
+                String[] boxMeasurements = boxLine.split("\\s+");
+
+                Box box = new Box(numberOfDimensions);
+                box.index = i + 1; //Boxes count starts with 1
+
+                for (int j = 0; j < numberOfDimensions; j++) {
+                    box.measurements[j] = Integer.parseInt(boxMeasurements[j]);
+                }
+
+                stackingBoxes.addBox(box);
+            }
+
+            stackingBoxes.findLongestNestingString();
+
+            out.printf("%d\n", stackingBoxes.numberOfBoxesInLongestString());
+            out.printf("%s\n", stackingBoxes.boxesInLongestString());
+        }
+
+    }
+
+}
