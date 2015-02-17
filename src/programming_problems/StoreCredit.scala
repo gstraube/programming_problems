939029265ff5882bb3a4212a13e9bd3bfbe46ee8
gstraube <georg.straube@googlemail.com>
2015-02-17 20:42:44 +0100

    Code skeleton for the store credit problem.

---
+++ src/programming_problems/StoreCredit.scala
@@ -0,0 +1,32 @@
+object StoreCredit {
+
+  def readInput(): Seq[Int] = List(150, 24, 79, 50, 88, 345, 3)
+
+  def findItems(items: Seq[Int]): (Int, Int) = {
+    val sortedItems = items.sortWith(_<_)
+
+    val firstItemIndex = 0
+    val secondItemIndex = 0
+    /*
+        Extract initial median value
+
+        Calculate difference between target sum and the median
+
+        If the difference is less or equal than the median,
+        search recursively in the left half of sortedItems. Otherwise search
+        in the right half.
+
+        If a matching item is not found, proceed with the item to the right.
+
+     */
+
+    (firstItemIndex, secondItemIndex)
+  }
+
+  def main(args: Array[String]) {
+    val items = readInput()
+    val (firstItem, secondItem) = findItems(items)
+    println(firstItem, secondItem)
+  }
+
+}
