aa9fcafb1f06bbc7d7334e2aa65e9d7bbe125bd6
gstraube <georg.straube@googlemail.com>
2015-02-18 21:28:55 +0100

    Implemented algorithm.

--- src/programming_problems/StoreCredit.scala
+++ src/programming_problems/StoreCredit.scala
@@ -4,32 +4,66 @@
 
 object StoreCredit {
 
-  def readInput(): Seq[Int] = List(150, 24, 79, 50, 88, 345, 3)
+  def readInput(): Seq[Int] = Vector(150, 24, 79, 50, 88, 345, 3)
 
-  def findItems(items: Seq[Int]): (Int, Int) = {
-    val sortedItems = items.sortWith(_<_)
+  def findItems(items: Seq[Int], targetSum: Int): (Int, Int) = {
+    val sortedItems = items.sortWith(_ < _)
 
-    val firstItemIndex = 0
-    val secondItemIndex = 0
-    /*
-        Extract initial median value
+    var foundSecondItem = false
 
-        Calculate difference between target sum and the median
+    var firstItemIndex = Math.floor(sortedItems.size.toDouble / 2.0).toInt
+    var firstItem = sortedItems(firstItemIndex)
 
-        If the difference is less or equal than the median,
-        search recursively in the left half of sortedItems. Otherwise search
-        in the right half.
+    var secondItem = firstItem
 
-        If a matching item is not found, proceed with the item to the right.
+    while (!foundSecondItem) {
 
-     */
+      firstItem = sortedItems(firstItemIndex)
+      val difference = targetSum - firstItem
 
-    (firstItemIndex, secondItemIndex)
+      val (foundItem, item) = findSecondItem(sortedItems, difference)
+
+      foundSecondItem = foundItem
+      secondItem = item
+
+      firstItemIndex = firstItemIndex - 1
+    }
+
+    (firstItem, secondItem)
+  }
+
+  def findSecondItem(items: Seq[Int], difference: Int): (Boolean, Int) = {
+
+    val medianIndex = Math.floor(items.size.toDouble / 2.0).toInt
+    val median = items(medianIndex)
+
+    if (items.size == 1) {
+      if (median == difference) {
+        return (true, median)
+      } else {
+        return (false, median)
+      }
+    }
+
+    if (difference == median) {
+      return (true, median)
+    }
+
+    var startIndex = 0
+    var endIndex = items.size - 1
+
+    if (difference < median) {
+      endIndex = medianIndex - 1
+    } else {
+      startIndex = medianIndex + 1
+    }
+
+    findSecondItem(items.slice(startIndex, endIndex + 1), difference)
   }
 
   def main(args: Array[String]) {
     val items = readInput()
-    val (firstItem, secondItem) = findItems(items)
+    val (firstItem, secondItem) = findItems(items, 200)
     println(firstItem, secondItem)
   }
 
